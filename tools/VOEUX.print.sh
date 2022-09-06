12f35fd3aa9213549e9d776f1304d4473851bcd0
fred <support@qo-op.com>
2022-09-06 16:23:08 +0200

    ## Get Voeu title (pepper) = simple GUI form + Name collision => Voeu fusion

--- tools/VOEUX.print.sh
+++ tools/VOEUX.print.sh
@@ -20,23 +20,30 @@ for v in $(cat ~/.zen/game/players/*/voeux/*/.title); do
     g1pub=$(grep -r $v ~/.zen/game/players/*/voeux/ $v 2>/dev/null | rev | cut -d '/' -f 2 | rev )
     echo "$v : $g1pub"
     echo '------------------------------------------------------------------'
+    vlist=($v:$g1pub ${vlist[@]})
 done
+
+echo "${vlist[@]}"
+
 
 PS3='Choisissez le voeux ___ '
 voeux=($(ls ~/.zen/game/players/$PLAYER/voeux 2>/dev/null) "QUITTER")
 
-select voeu in "${voeux[@]}"; do
+select voeu in "${vlist[@]}"; do
     case $voeu in
     "QUITTER")
         exit 0
     ;;
 
     *) echo "IMPRESSION $voeu"
+        voeu=$(echo $voeu | cut -d ':' -f2) ## Get G1PUB part
+
         myIP=$(hostname -I | awk '{print $1}' | head -n 1)
         VOEUXNS=$(ipfs key list -l | grep $voeu | cut -d ' ' -f1)
+
         qrencode -s 12 -o "$HOME/.zen/game/world/$voeu/QR.WISHLINK.png" "http://$myIP:8080/ipns/$VOEUXNS"
         convert $HOME/.zen/game/world/$voeu/QR.WISHLINK.png -resize 600 ~/.zen/tmp/QRWISHLINK.png
-        TITLE=$(cat ~/.zen/game/world/$voeu/.pepper)
+        TITLE=$(cat ~/.zen/game/world/$voeu/.pepper) ## Get Voeu title (pepper) = simple GUI form + Name collision => Voeu fusion
         convert -gravity northwest -pointsize 40 -fill black -draw "text 50,2 \"$TITLE\"" ~/.zen/tmp/QRWISHLINK.png ~/.zen/tmp/g1voeu1.png
         convert -gravity southeast -pointsize 40 -fill black -draw "text 50,2 \"$TITLE\"" ~/.zen/tmp/g1voeu1.png ~/.zen/tmp/g1voeu.png
 
