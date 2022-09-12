5e80ddcafd235cfbc3a56f90d3724b12919b2e03
fred <support@qo-op.com>
2022-09-12 11:36:39 +0200

    Add duration time display

--- 20h12.process.sh
+++ 20h12.process.sh
@@ -6,8 +6,14 @@
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 ME="${0##*/}"
-
+start=`date +%s`
 ## PROCESS TW BACKOFFICE TREATMENT
 
 ~/.zen/Astroport.ONE/ASTROBOT/VOEUX.refresh.sh
 ~/.zen/Astroport.ONE/ASTROBOT/PLAYER.refresh.sh
+
+
+########################################################################
+end=`date +%s`
+echo Execution time was `expr $end - $start` seconds.
+exit 0
