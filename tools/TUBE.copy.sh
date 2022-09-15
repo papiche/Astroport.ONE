dd540354816f84e34ebf04a829d92814be9e4bc0
fred <support@qo-op.com>
2022-09-15 21:51:51 +0200

    clean title

--- tools/TUBE.copy.sh
+++ tools/TUBE.copy.sh
@@ -36,12 +36,12 @@ for yurl in $(cat ~/.zen/tmp/tiddlers.json | jq -r '.[].text' | grep 'http'); do
 
 
         TITLE=$(yt-dlp --print title ${yurl})
-        TITLE=$(echo "$TITLE" | tr -cd '[:alnum:]._-')
+        TITLE=${TITLE//[^a-zA-Z0-9 ]/}
 
         # https://github.com/yt-dlp/yt-dlp#format-selection-examples
         # SUBS ? --write-subs --write-auto-subs --sub-langs "en, en-orig" --embed-subs
         # TODO : DELAY COPY OPERATION...  Astro can download quicker at 03:00 AM
-        echo "yt-dlp -f \"bv*[ext=mp4][height<=480]+ba/b[height<=480] / bv*[ext=mp4][height<=720]+ba/b[height<=720]\" --no-mtime --embed-thumbnail --add-metadata -o \"$HOME/.zen/tmp/tube/%(title)s.%(ext)s\" ${yurl}"
+        echo "yt-dlp -f \"bv*[ext=mp4][height<=480]+ba/b[height<=480] / bv*[ext=mp4][height<=720]+ba/b[height<=720]\" --no-mtime --embed-thumbnail --add-metadata -o \"$HOME/.zen/tmp/tube/$TITLE.%(ext)s\" ${yurl}"
         yt-dlp -f "bv*[ext=mp4][height<=480]+ba/b[height<=480] / bv*[ext=mp4][height<=720]+ba/b[height<=720]" -S "filesize:500M" --no-mtime --embed-thumbnail --add-metadata -o "$HOME/.zen/tmp/tube/$TITLE.%(ext)s" ${yurl}
 
         echo
