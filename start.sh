c9a631dd6e7687df68cbb1a00f0474125f07f43c
fred <support@qo-op.com>
2022-10-26 18:58:49 +0200

    Restart 12345.sh process and tail log

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ ME="${0##*/}"
 TS=$(date -u +%s%N | cut -b1-13)
 MOATS=$(date -u +"%Y%m%d%H%M%S%4N")
 
-killall 12345.sh; killall nc
+killall 12345.sh; killall nc; killall tail
 
 echo '
     _    ____ _____ ____   ___  ____   ___  ____ _____    ___  _   _ _____
@@ -35,8 +35,9 @@ YOU=$(ps auxf --sort=+utime | grep -w ipfs | grep -v -E 'color=auto|grep' | tail
 [[ ! $YOU ]] && echo "Lancez 'ipfs daemon' SVP sudo systemctl start ipfs" && exit 1
 IPFSNODEID=$(cat ~/.ipfs/config | jq -r .Identity.PeerID)
 
-echo "LAUNCHING 1234 PORT REDIRECTING SPIDER http://$myIP:1234"
 ~/.zen/Astroport.ONE/12345.sh > ~/.zen/tmp/12345.log &
+tail -f ~/.zen/tmp/12345.log &
+echo "1234 PORT SPIDER http://$myIP:1234"
 
 ## CREATE AND OR CONNECT USER
     PS3='Créez VISA ou connectez-vous à votre compte Astronaute ___ '
