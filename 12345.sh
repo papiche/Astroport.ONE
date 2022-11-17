e294ef155f0df63401b14909d40ef16537229c2f
fred <support@qo-op.com>
2022-11-17 19:13:50 +0100

    REDIRECT in case Already existing TW

--- 12345.sh
+++ 12345.sh
@@ -502,7 +502,8 @@ echo "" > ~/.zen/tmp/.ipfsgw.bad.twt # TODO move in 20h12.sh
                     $MY_PATH/tools/mailjet.sh "${WHAT}" ~/.zen/tmp/email.${WHAT}.${MOATS}.txt
                     ) &
 
-                    echo "$HTTPCORS - BOOTING - ASTRONAUT $PSEUDO - IPFS FORMATING - [$SALT + $PEPPER] (${WHAT})
+                    echo "$HTTPCORS -    <meta http-equiv='refresh' content='3; url=\"http://"$myIP":8080/ipns/"$ASTRONAUTENS"\"'/>
+                    <h1>BOOTING - ASTRONAUT $PSEUDO </h1> IPFS FORMATING - [$SALT + $PEPPER] (${WHAT})
                     <br>- TW - http://$myIP:8080/ipns/$ASTRONAUTENS <br> - GW - /ipns/$IPFSNODEID" | nc -l -p ${PORT} -q 1 > /dev/null 2>&1 &
                      echo "(☓‿‿☓) Execution time was "`expr $end - $start` seconds.
                     continue
