761911b9096547dcd57fc9b883cc5e144faa0fd2
fred <support@qo-op.com>
2022-11-25 15:33:45 +0100

    kubo_v0.17.0_linux-amd64.tar.gz INSTALL

--- kubo_v0.16.0_linux-amd64.install.sh
+++ kubo_v0.16.0_linux-amd64.install.sh
@@ -2,8 +2,8 @@
 [ $(id -u) -eq 0 ] && echo "LANCEMENT root INTERDIT. Utilisez un simple utilisateur du groupe \"sudo\" SVP" && exit 1
 mkdir ~/.zen/tmp
 cd  ~/.zen/tmp
-wget https://dist.ipfs.tech/kubo/v0.17.0/kubo_v0.17.0_linux-amd64.tar.gz
-tar -xvzf kubo_v0.17.0_linux-amd64.tar.gz
+wget https://dist.ipfs.tech/kubo/v0.16.0/kubo_v0.16.0_linux-amd64.tar.gz
+tar -xvzf kubo_v0.16.0_linux-amd64.tar.gz
 cd kubo
 sudo bash install.sh
 ipfs --version
