05f1a9b4c02c3b82d37a491dcfe9331318bc2639
Yann Autissier <yann.autissier@gmail.com>
2022-12-26 18:33:19 +0000

    fix myHOST

--- tools/myhash.sh
+++ tools/myhash.sh
@@ -6,46 +6,50 @@ myHash() {
 	 || myHash=$(template_register |ipfs add -q)
 	[ ! -f ~/.zen/game/players/localhost/latest ] \
 	 && echo "$myHash" > ~/.zen/game/players/localhost/latest
-	[ -n "$myHash" ] \
-	 && echo "$myHash"
+	[ -n "$myHash" ] && echo "$myHash"
 }
 
 myHttp() {
-	echo "$(myHttpHeader)"
-	echo
-	echo "$(myHttpContent)"
+	[ -n "$(myHttpHeader)" ] \
+	 && echo "${myHttpHeader}" \
+	 && echo
+	[ -n "$(myHttpContent)" ] \
+	 && echo "${myHttpContent}"
+}
+
+myHttpContent() {
+	[ -n "$(myHash)" ] \
+	 && myHttpContent="<html><head><title>302 Found</title></head><body><h1>Found</h1>
+<p>The document is <a href=\""ipfs/$(myHash)"\">here</a> in IPFS.</p></body></html>" \
+	 && echo "$myHttpContent"
+}
+
+myHttpHeader() {
+	[ -n "$(myHash)" ] \
+	 && myHttpHeader="HTTP/1.0 302 Found
+Content-Type: text/html; charset=UTF-8
+Content-Length: $(myHttpContent |wc -c)
+Date: $(date -R)
+Location: ipfs/$(myHash)
+Server: and"
+	[ -n "$(myKey)" ] && myHttpHeader+="
+set-cookie: AND=$(myKey); expires=$(date -R -d "+1 month"); path=/; domain=.$myDomainName; Secure; SameSite=lax"
+	[ -n "$myHttpHeader" ] && echo "$myHttpHeader"
 }
 
 myIpfs() {
-	myIpfs=${myIPFS}/ipfs/$(myHash)
-	echo "$myIpfs"
+	[ -n "$(myHash)" ] \
+	 && myIpfs="${myIPFS}/ipfs/$(myHash)" \
+	 && echo "$myIpfs"
 }
 
 myIpns() {
-	myIpns=${myIPFS}/ipns/$(myKey)
-	echo "$myIpns"
+	[ -n "$(myKey)" ] \
+	 && myIpns="${myIPFS}/ipns/${myKey}" \
+	 && echo "$myIpns"
 }
 
 myKey() {
 	myKey=$(ipfs key list -l | awk '$2 == "self" {print $1}')
 	[ -n "$myKey" ] && echo "$myKey"
-}
-
-myHttpContent() {
-	myHash=$(myHash)
-	myHttpContent="<html><head><title>302 Found</title></head><body><h1>Found</h1>
-<p>The document has moved <a href="ipfs/$myHash">here</a>.</p></body></html>"
-	echo "$myHttpContent"
-}
-
-myHttpHeader() {
-	myHttpContent=$(myHttpContent)
-	myHttpHeader="HTTP/1.0 302 Found
-Content-Type: text/html; charset=UTF-8
-Content-Length: $(myHttpContent |wc -c)
-Date: $(date -R)
-Location: ipfs/$myHash
-set-cookie: AND=$myKey; expires=$(date -R -d "+1 month"); path=/; domain=.$myDomainName; Secure; SameSite=lax
-Server: and"
-	echo "$myHttpHeader"
 }
