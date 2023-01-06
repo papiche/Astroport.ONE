02ceb888aa0bfa9cd23bce4ac0c16057bb94cd66
fred <support@qo-op.com>
2023-01-06 22:20:17 +0100

    install ipfs kubo

---
+++ kubo_v0.16.0_linux.install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+[ $(id -u) -eq 0 ] && echo "LANCEMENT root INTERDIT. Utilisez un simple utilisateur du groupe \"sudo\" SVP" && exit 1
+
+mkdir ~/.zen/tmp
+cd  ~/.zen/tmp
+
+# Check processor architecture
+architecture=$(uname -m)
+
+# Download appropriate version of kubo
+if [ "$architecture" == "x86_64" ]; then
+    wget https://dist.ipfs.tech/kubo/v0.16.0/kubo_v0.16.0_linux-amd64.tar.gz
+elif [ "$architecture" == "aarch64" ]; then
+    wget https://dist.ipfs.tech/kubo/v0.16.0/kubo_v0.16.0_linux-arm64.tar.gz
+else
+    echo "Error: Unknown architecture"
+    exit 1
+fi
+
+tar -xvzf kubo_v0.16.0_linux-$architecture.tar.gz
+
+cd kubo
+
+# Install
+sudo bash install.sh
+
+# Test & clean
+[[ $(ipfs --version) ]] \
+&& rm -Rf ~/.zen/tmp/kubo*
