1e137bbe8287f0c5fc50af38b743a06497ae1bc7
fred <support@qo-op.com>
2023-10-26 01:34:32 +0200

    TITLE FORK TO BE SOLVED. NEED "SAME EMAILS SIGNATURES"

--- tools/RSS2UPlanetTW.sh
+++ tools/RSS2UPlanetTW.sh
@@ -64,12 +64,16 @@ while read title; do
         NTAGS=$(cat ~/.zen/tmp/${MOATS}/NEW.json | jq -r .tags)
         NEMAILS=($(echo "$NTAGS" | grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"))
         N=${#NEMAILS[@]}
+        echo "New Tiddler signatures : ${NEMAILS[*]}"
 
         ITAGS=$(cat ~/.zen/tmp/${MOATS}/INSIDE.json | jq -r .tags)
         IEMAILS=($(echo "$ITAGS" | grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b"))
         I=${#IEMAILS[@]}
+        echo "Inside Tiddler signatures : ${IEMAILS[*]}"
 
-        if [ ${N} -gt ${I} ]; then
+        ## NB: COULD NEED SORTING (TODO)
+
+        if [[ "${NEMAILS[*]}" != "${IEMAILS[*]}" ]; then
 
             ## DIFFERENCE IN EMAIL SIGNATURES
             COMMON=()
@@ -105,11 +109,11 @@ while read title; do
             for email in "${unique_combined[@]}"; do
 
 echo "Hello\n\n
-Tiddler with same title is existing in ${unique_combined[*]} TW(s) \n\n
+A tiddler with same title is existing in ${unique_combined[*]} TW(s) \n\n
 $title\n\n
 Please choose update your TW grabbing it from\n
-* ACTUAL: ${myIPFS}/ipfs/${INSIDETID}\n
-* NEW: ${myIPFS}/ipfs/${NEWTID}\n\n
+* ACTUAL : ${myIPFS}/ipfs/${INSIDETID}\n
+* NEW : ${myIPFS}/ipfs/${NEWTID}\n\n
 or fork modifying titles
 
 You can discuss about it in room ${MOATS}\n
@@ -120,7 +124,7 @@ https://vdo.copylaradio.com
 
             done
 
-        ## WAIT TITLE FORK TO BE SOLVED
+        ##  TITLE FORK TO BE SOLVED. NEED "SAME EMAILS SIGNATURES"
         continue
 
         fi
