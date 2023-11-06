ba010ce3a7721f277ac46a03f476e9ddd270f8d6
fred <support@qo-op.com>
2023-11-06 13:46:56 +0100

    Make your adventure

--- adventure/rooms/meteofrance.sh
+++ adventure/rooms/meteofrance.sh
@@ -8,18 +8,21 @@ ts=$(date -u +%s%N | cut -b1-13)
 ################################################################################
 # Capture la photographie satellite de la France
 # https://fr.sat24.com/image?type=visual5HDComplete&region=fr
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+ME="${0##*/}"
 
-mkdir -p ~/..zen/adventure/meteo.anim.eu
-rm -f ~/..zen/adventure/meteo.anim.eu/meteo.png
-curl  -m 20 --output ~/..zen/adventure/meteo.anim.eu/meteo.png https://s.w-x.co/staticmaps/wu/wu/satir1200_cur/europ/animate.png
+mkdir -p ~/.zen/adventure/meteo.anim.eu
+rm -f ~/.zen/adventure/meteo.anim.eu/meteo.png
+curl  -m 20 --output ~/.zen/adventure/meteo.anim.eu/meteo.png https://s.w-x.co/staticmaps/wu/wu/satir1200_cur/europ/animate.png
 
-if [[ ! -f  ~/..zen/adventure/meteo.anim.eu/meteo.png ]]; then
+if [[ ! -f  ~/.zen/adventure/meteo.anim.eu/meteo.png ]]; then
     echo "Impossible de vous connecter au service meteo"
     exit 1
 else
     echo "NEED HTML TEMPLATING"
     echo "Mise à jour archive points meteo : $ts"
-    echo $ts > ~/..zen/adventure/meteo.anim.eu/.ts
+    echo $ts > ~/.zen/adventure/meteo.anim.eu/.ts
 
     PSEUDO=$(cat ~/.zen/adventure/players/.current/.pseudo 2>/dev/null)
     OLDID=$(cat ~/.zen/adventure/players/.current/.meteo.index 2>/dev/null)
@@ -34,8 +37,8 @@ else
     INDEXID=$(ipfs add -rHq ~/.zen/adventure/players/.current/public | tail -n 1)
     echo $INDEXID > ~/.zen/adventure/players/.current/.meteo.index
     echo "LAST VIDEO INDEX http://127.0.0.1:8080/ipfs/$INDEXID"
-    IPFS=$(ipfs add -Rw ~/..zen/adventure/meteo.anim.eu/)
-    echo $IPFS > ~/..zen/adventure/meteo.anim.eu/.chain
+    IPFS=$(ipfs add -Rw ~/.zen/adventure/meteo.anim.eu/)
+    echo $IPFS > ~/.zen/adventure/meteo.anim.eu/.chain
 
 
 
