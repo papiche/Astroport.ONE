4f6be54321b7c7dbe1ecc54345b5e36b05587a10
fred <support@qo-op.com>
2023-11-13 00:40:17 +0100

    fred@g1sms.fr BCC

--- tools/mailjet.sh
+++ tools/mailjet.sh
@@ -18,7 +18,7 @@ echo '
 ### PLEASE CHANGE YOUR DOMAIN AND KEY ( OR HELP PAYING TRAFIC ;)
 ## THIS IS A FREE LIMITED ACCOUNT. DO NOT EXAGERATE ;)
 mail="$1" # EMAIL DESTINATAIRE
-[[ ! $1 ]] && mail="support@g1sms.fr"
+[[ ! $1 ]] && mail="fred@g1sms.fr"
 
 messfile="$2" # FICHIER A AJOUTER AU CORPS MESSAGE
 
@@ -33,7 +33,7 @@ echo "
 
 echo "From: support@g1sms.fr
 To: EMAIL
-Bcc: support@g1sms.fr
+Bcc: fred@g1sms.fr
 Subject: SUBJECT
 MESSAGE
 " > ~/.zen/tmp/email.txt
