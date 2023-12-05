9d6daccf29905eaefcd62158ab02c89b9c96f7ac
fred <support@qo-op.com>
2023-12-05 23:45:13 +0100

    corrected

--- tools/MOATS2seconds.sh
+++ tools/MOATS2seconds.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
-# ROUGHLY CONVERT MOATS IN SECONDS
-MOATS=$1
-[[ ${MOATS} == "" ]] && echo 0 & exit 1
-ZMOATS=${MOATS::-4}
+# ROUGHLY CONVERT moats IN SECONDS
+moats="$1"
+
+[[ ${moats} == "" ]] && echo 0 && exit 1
+Zmoats=${moats::-4}
 
 # Calculate the number of days in the specified month (for simplicity, assuming 30 days per month)
 DAYS_IN_MONTH=30
 
-# Calculate ZMOATS in seconds since the epoch manually
-YEAR=${ZMOATS:0:4}
+# Calculate Zmoats in seconds since the epoch manually
+YEAR=${Zmoats:0:4}
 SECYEAR=$((YEAR * 365 * 24 * 3600))
 
-MONTH=$((${ZMOATS:4:2}+0))
+MONTH=$((${Zmoats:4:2}+0))
 SECMONTH=$((MONTH * DAYS_IN_MONTH * 24 * 3600))
 
-DAY=$((${ZMOATS:6:2}+0))
+DAY=$((${Zmoats:6:2}+0))
 SECDAY=$((DAY * 24 * 3600))
 
-HOUR=$((${ZMOATS:8:2}+0))
+HOUR=$((${Zmoats:8:2}+0))
 SECHOUR=$((HOUR * 3600))
 
-MINUTE=$((${ZMOATS:10:2}+0))
+MINUTE=$((${Zmoats:10:2}+0))
 SECMINUTE=$((MINUTE * 60))
 
-SECOND=$((${ZMOATS:12:2}+0))
+SECOND=$((${Zmoats:12:2}+0))
 
 
 # Calculate the time difference in seconds
-ZMOATS_SECONDS=$((SECYEAR + SECMONTH + SECDAY + SECHOUR + SECMINUTE + SECOND))
+Zmoats_SECONDS=$(( SECYEAR + SECMONTH + SECDAY + SECHOUR + SECMINUTE + SECOND))
 
-echo "$ZMOATS_SECONDS"
+echo "$Zmoats_SECONDS"
 exit 0
+
