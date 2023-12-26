a7991fe9e794c8185545e9ac53c7b69c7028d8ec
fred <support@qo-op.com>
2023-12-26 17:59:05 +0100

    Zen

--- tools/make_image_ipfs_index_carousel.sh
+++ tools/make_image_ipfs_index_carousel.sh
@@ -98,7 +98,7 @@ if [[ ! -d $img_dir ]]; then
 
         ## WRITE ON IT : ASK FOR REFILL
         convert -font 'Liberation-Sans' \
-        -pointsize 80 -fill purple -draw 'text 50,120 "'"$ZEN Ẑen"'"' \
+        -pointsize 80 -fill purple -draw 'text 50,120 "'"$ZEN Zen"'"' \
         -pointsize 30 -fill purple -draw 'text 40, 180 "'"$PLAYER"'"' \
         -pointsize 14 -fill white -draw 'text 40, 200 "'"${birthdate}"'"' \
         "${HOME}/.zen/tmp/one.png" "${HOME}/.zen/tmp/carousel/${pub}.png" \
