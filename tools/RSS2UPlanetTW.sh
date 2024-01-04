e23c3bc1a63fb4791f20ba9bf80b530fb76a9249
fred <support@qo-op.com>
2024-01-04 12:56:16 +0100

    what is that bug :::

--- tools/RSS2UPlanetTW.sh
+++ tools/RSS2UPlanetTW.sh
@@ -57,14 +57,13 @@ while read title; do
         echo "Importing Title: $title"
         cat "${RSS}" | jq -rc ".[] | select(.title == \"$title\")" > ~/.zen/tmp/${MOATS}/${SECTOR}/NEW.json
 
-        cat ~/.zen/tmp/${MOATS}/${SECTOR}/NEW.json | jq
-
         echo "DEBUG"
+        #~ cat ~/.zen/tmp/${MOATS}/${SECTOR}/NEW.json | jq
         echo "tiddlywiki  --load ${INDEX} --import ~/.zen/tmp/${MOATS}/${SECTOR}/NEW.json 'application/json' --output ~/.zen/tmp/${MOATS}/${SECTOR} --render '$:/core/save/all' '"${SECTOR}.html"' 'text/plain'"
 
         tiddlywiki --load ${INDEX} \
-            --import ~/.zen/tmp/${MOATS}/${SECTOR}/NEW.json "application/json" \
-            --output ~/.zen/tmp/${MOATS}/${SECTOR} --render "$:/core/save/all" "${SECTOR}.html" "text/plain"
+            --import ~/.zen/tmp/${MOATS}/${SECTOR}/NEW.json 'application/json' \
+            --output ~/.zen/tmp/${MOATS}/${SECTOR} --render '$:/core/save/all' "${SECTOR}.html" 'text/plain'
 
         [[ -s ~/.zen/tmp/${MOATS}/${SECTOR}/${SECTOR}.html ]] \
             && rm ${INDEX} \
