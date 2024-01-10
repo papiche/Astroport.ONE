79977841468de63c784e57214f4c75940b697a1d
fred <support@qo-op.com>
2024-01-10 18:51:37 +0100

    crazy PC

--- test.sh
+++ test.sh
@@ -23,6 +23,12 @@ echo "jaklis can ACCESS BLOCKCHAIN"
 ${MY_PATH}/tools/jaklis/jaklis.py history -p ${WORLDG1PUB}
 jaklis=$?
 
+test=$tw$ipfs$keygen$jaklis
 
-echo $tw $ipfs $keygen $jaklis
+[[ $test == "0000" ]] && echo "PERFECT" && exit 0
+
+[[ ${test::1} == "1" ]] && echo "PROBLEM WITH TiddlyWiki"
+[[ ${test:2:2} == "11" ]] && echo "CRYPTO LAYER MALFUNCTION"
+[[ ${test::2} == "01" ]] && echo "IPFS DAEMON IS ABSENT"
+
 exit 0
