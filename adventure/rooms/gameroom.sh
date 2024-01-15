dc0e9c8053f45f298cf26db576620d3197392a3b
fred <support@qo-op.com>
2024-01-15 11:36:56 +0100

    route missing

--- adventure/rooms/gameroom.sh
+++ adventure/rooms/gameroom.sh
@@ -21,7 +21,7 @@ echo "y est raccordé...  D'autres fils sortent de l'appareil. Une webcam. Une i
 echo
 echo "Que voulez vous faire?"
 myIP=$(hostname -I | awk '{print $1}' | head -n 1)
-isLAN=$(route -n |awk '$1 == "0.0.0.0" {print $2}' | grep -E "/(^127\.)|(^192\.168\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^::1$)|(^[fF][cCdD])/")
+isLAN=$(echo $myIP | grep -E "/(^127\.)|(^192\.168\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^::1$)|(^[fF][cCdD])/")
 [[ ! $myIP || $isLAN ]] && myIP="127.0.1.1"
 
 while true; do
