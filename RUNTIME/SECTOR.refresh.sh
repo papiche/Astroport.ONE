8503605c212ebd59b7475159ac6029f5313edef4
fred <support@qo-op.com>
2024-01-17 20:33:14 +0100

    ## TODO get from ~/.zen/tmp/${IPFSNODEID}/REGIONS/_${REGLAT}_${REGLON}.week.cid produced by REGION.refresh

--- RUNTIME/SECTOR.refresh.sh
+++ RUNTIME/SECTOR.refresh.sh
@@ -243,12 +243,14 @@ for SECTOR in ${SECTORS[@]}; do
                   -e "s~_ZONENS_~${SECTORNS}~g" \
                   -e "s~_UPZONENS_~${REGIONNS}~g" \
                   -e "s~_SECTORG1PUB_~${SECTORG1PUB}~g" \
-                  -e "s~_RWEEKCID_~/ipfs/QmY4rLRgSXjhhsW3WoKUJYpLe2A1dJCFXb7stNLcAp95w9~g" \
                   -e "s~_PHONEBOOTH_~${PHONEBOOTH}~g" \
                   -e "s~_DATE_~$(date +%A-%d_%m_%Y)~g" \
                   -e "s~_UPLANETLINK_~${EARTHCID}/map_render.html\?southWestLat=${REGLAT}\&southWestLon=${REGLON}\&deg=0.1~g" \
                   -e "s~http://127.0.0.1:8080~~g" \
         > ~/.zen/tmp/${MOATS}/${SECTOR}/_index.html
+
+## TODO get from ~/.zen/tmp/${IPFSNODEID}/REGIONS/_${REGLAT}_${REGLON}.week.cid produced by REGION.refresh
+#      -e "s~_RWEEKCID_~/ipfs/QmY4rLRgSXjhhsW3WoKUJYpLe2A1dJCFXb7stNLcAp95w9~g" \
 
         ##################################
         cp -f ~/.zen/tmp/${MOATS}/${SECTOR}/_index.html ~/.zen/tmp/${MOATS}/${SECTOR}/index.html
