6aa2ad05b97738b17a4cddd0b670ed45138a54d2
fred <support@qo-op.com>
2024-02-02 20:11:54 +0100

    update

--- tools/jaklis/setup.sh
+++ tools/jaklis/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-MY_PATH="`dirname \"$0\"`"              # relative
-MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
-for i in gcc python3-pip python3-setuptools libpq-dev python3-dev python3-wheel python3-duniterpy python3-termcolor  ; do
+hasError=0
+
+for i in gcc python3-pip python3-setuptools libpq-dev python3-dev python3-wheel; do
     if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
         [[ ! $j ]] && sudo apt update
         sudo apt install -y $i
@@ -10,11 +10,12 @@ for i in gcc python3-pip python3-setuptools libpq-dev python3-dev python3-wheel 
     fi
 done
 
-python -m pip install -r requirements.txt
+pip3 install -r requirements.txt || hasError=1
+chmod u+x jaklis.py
+sudo ln -sf $(realpath jaklis.py) /usr/local/bin/jaklis || hasError=1
 
-## INSTALL SYSTEM WIDE
-mkdir -p ~/.local/bin/
-
-cp ${MY_PATH}/jaklis.py ~/.local/bin/jaklis
-cp ${MY_PATH}/.env ~/.local/bin/
-chmod u+x ~/.local/bin/jaklis
+if [[ hasError -eq 0 ]]; then
+    echo "Setup done. You can use 'jaklis' command, try it."
+else
+    echo "An error has occurred"
+fi
