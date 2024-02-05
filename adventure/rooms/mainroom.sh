1ed34c1ccf074161fb9f4f483132665f4856a4d2
jonathanbordes <119431732+jonathanbordes@users.noreply.github.com>
2024-02-05 10:20:50 +0100

    Update mainroom.sh

--- adventure/rooms/mainroom.sh
+++ adventure/rooms/mainroom.sh
@@ -21,6 +21,17 @@ echo "Vous ne pouvez pas vraiment en imaginer la taille,"
 echo
 echo "Vous pouvez vous diriger au nord, à l'est, au sud et à l'ouest."
 echo
+echo "Un terminal informatique est installé là."
+echo
+echo "Il ressemble à une grosse calculatrice"
+echo
+leverstate=`cat ../logic/leverlogic.ben`
+            if [ "$leverstate" = "on" ]; then
+                echo "'VISA SVP' clignote sur l'écran..."
+            else
+                echo "La machine affiche l'heure : 20:12"
+            fi
+echo
 echo "Que voulez-vous faire ?"
 
 # And the room logic once again.
@@ -35,8 +46,18 @@ while true; do
             exit ;;
         w ) ./green.sh
             exit ;;
-        u ) echo "Il n'y a rien que vous puissiez utiliser ici." ;;
-        h ) echo "Vous observez votre montre, il est 20:12" ;;
+        u ) leverstate=`cat ../logic/leverlogic.ben`
+            if [ "$leverstate" = "on" ]; then
+                echo "A chaque frappe d'une touche. l'écran fait défiler le texte 'SCANNEZ VISA SVP'."
+            else
+                sed -i='' 's/off/on/' ../logic/leverlogic.ben
+                echo "Vous pianotez sur l'appareil..."
+                sleep 3
+                echo "A moment où vous touchez la touche '#' L'écran se met à clignoter..."
+                echo "Puis le message 'ACTIVATION STATION' défile sur les caractères lumineux."
+            fi
+        ;;
+        h ) echo "Le terminal comporte un clavier numérique. Un petit écran.. Il est réalisé avec un mini ordinateur Raspberry Pi. Il porte l'adresse G1TAG [https://g1sms.fr]" ;;
         * ) echo "Je suis désolé, je ne vous comprends pas. Les commandes sont : n, e, s, w, u et h..";;
     esac
 done
