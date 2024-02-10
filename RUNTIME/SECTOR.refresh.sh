fff9ae8d31bdfa1a5ac4b60228ee13508a2e1b68
fred <support@qo-op.com>
2024-02-10 15:58:06 +0100

    azimut

--- RUNTIME/SECTOR.refresh.sh
+++ RUNTIME/SECTOR.refresh.sh
@@ -253,7 +253,7 @@ for SECTOR in ${SECTORS[@]}; do
                   -e "s~_LON_~${REGLON}~g" \
                   -e "s~_EARTHCID_~${EARTHCID}~g" \
                   -e "s~_DATE_~$(date +%A-%d_%m_%Y)~g" \
-                  -e "s~_UPLANETLINK_~${EARTHCID}/map_render.html\?southWestLat=${REGLAT}\&southWestLon=${REGLON}\&deg=0.1~g" \
+                  -e "s~_UPLANETLINK_~${EARTHCID}/map_render.html\?southWestLat=${REGLAT}\&southWestLon=${REGLON}\&deg=1~g" \
                   -e "s~http://127.0.0.1:8080~~g" \
         > ~/.zen/tmp/${MOATS}/${SECTOR}/_index.html
 
