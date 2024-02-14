97677b4024afd44e913784a29ceefdccbfb200ac
fred <support@qo-op.com>
2024-02-14 15:04:48 +0100

    PING & CONNECT to Boostrap && /swarm/*

--- ping_bootstrap.sh
+++ ping_bootstrap.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
+## SHOW DHT STATS
+echo "------------------------------------------------- ~/.zen/tmp/ipfs.stats.dht.wan"
+echo "GETTING DHT STATS"
 ipfs stats dht wan > ~/.zen/tmp/ipfs.stats.dht.wan
-cat ~/.zen/tmp/ipfs.stats.dht.wan
+# cat ~/.zen/tmp/ipfs.stats.dht.wan
 
+## BOOSTRAP
+echo "-------------------------------------------------"
+echo "SWARM NODES"
 for bootnode in $(cat ~/.zen/Astroport.ONE/A_boostrap_nodes.txt | grep -Ev "#" | grep -v '^[[:space:]]*$')
-    do
-        echo
-        ipfsnodeid=${bootnode##*/}
-        ipfs swarm peers | grep $bootnode
-        ipfs ping -n 3 $bootnode
-        [ $? = 0 ] && ipfs swarm connect $bootnode \
-                        || echo "BAD NODE $bootnode"
-        echo "*****"
+do
+    echo
+    ipfsnodeid=${bootnode##*/}
+    ipfs swarm peers | grep $bootnode
+    ipfs --timeout 15s ping -n 3 $bootnode
+    [ $? = 0 ] && ipfs swarm connect $bootnode \
+                    || echo "BAD NODE $bootnode"
+    echo "*****"
+    echo "in DHT ? --------------"
+    cat ~/.zen/tmp/ipfs.stats.dht.wan | grep $ipfsnodeid
+    echo "-------------------------------------------------"
 
-        cat ~/.zen/tmp/ipfs.stats.dht.wan | grep $ipfsnodeid
+done
 
-    done
-
+## SWARM
+echo
+echo "-------------------------------------------------"
+echo "SWARM NODES"
+for ipfsnodeid in $(ls ~/.zen/tmp/swarm);
+do
+    ipfs --timeout 15s ping -n 3 /p2p/$ipfsnodeid
+    [ $? = 0 ] && ipfs swarm connect /p2p/$ipfsnodeid \
+                    || echo "BAD NODE $ipfsnodeid"
+    echo "in DHT ? --------------"
+    cat ~/.zen/tmp/ipfs.stats.dht.wan | grep $ipfsnodeid
+    echo "-------------------------------------------------"
+done
