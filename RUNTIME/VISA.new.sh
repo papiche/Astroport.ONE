9cad79fdae0e3fbbad3678cf62b90cfd11187ac5
fred <support@qo-op.com>
2024-03-12 15:21:39 +0100

    The 1st {{Astroport|astronautens}} TWs in Web3

--- RUNTIME/VISA.new.sh
+++ RUNTIME/VISA.new.sh
@@ -35,7 +35,7 @@ LIBRA=$(head -n 2 ${MY_PATH}/../A_boostrap_nodes.txt | tail -n 1 | cut -d ' ' -f
 ################################################################################
 #~ TWMODEL="/ipfs/bafybeid7xwuqkgyiffehs77x3wky3dghjncxepr5ln6dewapgvbwrqi7n4"
 #~ # ipfs cat $TWMODEL > templates/twdefault.html
-TWUPLANET="/ipfs/bafybeiarhdwmbvdh53hl5pwrzdeevdk5oq6qohgbt5qcodzcv2x5xwfana"
+TWUPLANET="/ipfs/bafybeif6kk7vyns6awm4gcuhwz32h6qvycha4b6qrjudort74jdn6ucocy"
 # ipfs cat $TWUPLANET > templates/twuplanet.html
 ################################################################################
 
