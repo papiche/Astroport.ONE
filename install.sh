1645c082c80924aa424b558a6ba307c1cd59eb27
fred <support@qo-op.com>
2024-04-21 14:03:59 +0200

    install in $HOME/.local/bin/jaklis/ (with .env for duniter load balancing)

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ start=`date +%s`
 
 [[ ! $(which ipfs) ]] && echo "INSTALL IPFS PLEASE" && exit 1
 #################################################################### TEST
+# tldr + mosquitto + promoetheus
 
 # MAIN # SI AUCUNE CLEF DE STATION...
 if [[ ! -d ~/.zen/game/players/ ]];
