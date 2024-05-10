5a08a1563d21a2c8a49424fe475ce31e07b53ec2
fred <support@qo-op.com>
2024-05-10 16:00:48 +0200

    MULTIPASS : HackerSpace Diplomatic Passport CCbySA / Mitch Altman / Matthew Borgatti / Frederic Renault

---
+++ templates/UPlanetZINE/day_/script.js
@@ -0,0 +1,4 @@
+// This script just adds a little print button to the page. It's not needed for the zine, unless you want to add one too!
+document.querySelector(".print-button").addEventListener("click", () => {
+  window.print();
+});
