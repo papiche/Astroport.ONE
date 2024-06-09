ec2093d5bf7222da3c2234fbd1d9f168961f42e8
fred <support@qo-op.com>
2024-06-09 19:49:30 +0200

    https://vdo.copylaradio.com connecting webRTC mesh

--- RUNTIME/TW/import_tiddler.sh
+++ RUNTIME/TW/import_tiddler.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# This Bash script automates updating a TiddlyWiki instance with new or modified content items (Tiddlers). It performs the following steps:
+# 1. Generates a unique timestamp to track updates.
+# 2. Verifies the existence of both the main TiddlyWiki index file and individual Tiddler JSON files, exiting with an error if either is missing.
+# 3. Incor CVs, adds 'created' and 'modified' fields from the unique timestamp to each Tiddler.
+# 4. Imports the updated Tiddlers into the wiki using TiddlyWiki's import command, outputting a temporary HTML file for review.
+# 5. Checks if the import was successful by verifying that the corresponding HTML file has been generated. On success:
+#    - Moves the new HTML file to replace the old index file in its original location.
+#    - Removes temporary files created during processing.
+# If any step fails, it provides an error message and exits with a non-zero status code.
 
 # Generate a unique timestamp
 MOATS=$(date -u +"%Y%m%d%H%M%S%4N")
@@ -17,7 +26,7 @@ TIDDLER="$2"
 
 # Add created and modified fields to the Tiddler JSON file
 echo "Putting ${TIDDLER} in ${TW}"
-jq '.[] + {created: $MOATS, modified: $MOATS}' --arg MOATS "$MOATS" "$TIDDLER" > "${TIDDLER}.tmp"
+cat "$TIDDLER" | jq --arg MOATS "$MOATS" '.[] + {created: $MOATS, modified: $MOATS}' > "${TIDDLER}.tmp"
 
 # Run TiddlyWiki import command
 echo "Running TiddlyWiki import..."
