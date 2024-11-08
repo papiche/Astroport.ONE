9ab07dddbb850a9467b3270ef8b5dd5a5a023716
fred <support@qo-op.com>
2024-11-08 21:40:45 +0100

    sapce

--- 12345.sh
+++ 12345.sh
@@ -143,7 +143,7 @@ while true; do
     if [[ $URL == "/" || $URL == "" ]]; then
         echo "/ CONTACT :  $HOSTP"
 
-        if [[ -z "$isLAN"  || $myASTROPORT != "" ]]; then
+        if [[ -z "$isLAN" || $myASTROPORT != "" ]]; then
         echo ${myASTROPORT}/${PORT}
         mySalt | \
             sed "s~http://127.0.0.1:12345~http://${myIP}:${PORT}~g" | \
