c3da80af4f2d61b755c4fd8136ba714fb4624d32
fred <support@qo-op.com>
2025-02-06 20:18:18 +0100

    create .env from my.sh

--- install_upassport.sh
+++ install_upassport.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 ## INSTALL UPassport
+################################################################################
+MY_PATH="`dirname \"$0\"`"
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+. "${MY_PATH}/tools/my.sh"
+
 echo "INSTALLING UPassport API : http://localhost:54321"
 if [[ ! -d ~/.zen/UPassport ]]; then
     mkdir -p ~/.zen
     cd ~/.zen
     git clone https://github.com/papiche/UPassport.git
     cd UPassport && ./setup_systemd.sh
+    ## CREATE .env
+cat > .env <<EOL
+myDUNITER="https://g1.cgeek.fr"
+myCESIUM="https://g1.data.e-is.pro"
+ipfsNODE="$myIPFS"
+CESIUMAPPIPFS="$CESIUMIPFS"
+OBSkey="null"
+EOL
+
     cd -
 fi
