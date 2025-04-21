c700e851b4737b4488701fef2c7c2cc8603a643f
fred <support@qo-op.com>
2025-04-21 15:44:18 +0200

    SALT PEPPER

--- tools/make_NOSTRCARD.sh
+++ tools/make_NOSTRCARD.sh
@@ -156,6 +156,8 @@ if [[ $EMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
             -e "s~Qmeu1LHnTTHNB9vex5oUwu3VVbc7uQZxMb8bYXuX56YAx2~${VAULTNSQR}~g" \
             -e "s~_NSECTAIL_~${NPRIV: -33}~g" \
             -e "s~_NOSTRVAULT_~/ipns/${NOSTRNS}~g" \
+            -e "s~_SALT_~${SALT}~g" \
+            -e "s~_PEPPER_~${PEPPER}~g" \
             -e "s~_MYRELAY_~${myRELAY}~g" \
             -e "s~_uSPOT_~${uSPOT}~g" \
             -e "s~_CAPTAINEMAIL_~${CAPTAINEMAIL}~g" \
