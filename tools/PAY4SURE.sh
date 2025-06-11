c771a86b172aa2af211a6f997599c8985c624da6
fred <support@qo-op.com>
2025-06-11 19:39:19 +0200

    logg

--- tools/PAY4SURE.sh
+++ tools/PAY4SURE.sh
@@ -55,21 +55,8 @@ echo "${ISSUERPUB} : (${AMOUNT}) -> ${G1PUB}"
 
 [[ -z $COMMENT ]] && COMMENT="UPLANET${UPLANETG1PUB:0:8}:ZEN:${MOATS}"
 
-PENDINGDIR=$HOME/.zen/game/pending/${ISSUERPUB}
-### PREPARE PENDINGFILE INFO ZONE
-mkdir -p ${PENDINGDIR}
-PENDINGFILE=${PENDINGDIR}/${MOATS}_${AMOUNT}+${G1PUB}.TX
-
-rm -f ${PENDINGFILE} 2>/dev/null ## CLEAN START
-
-## PREPARE CALLING MYSELF AGAIN COMMAND
-cp ${KEYFILE} ${PENDINGDIR}/${MOATS}.key 2>/dev/null
-echo '#!/bin/bash
-bash '${ME}' "'${PENDINGDIR}'/'${MOATS}'.key" "'${AMOUNT}'" "'${G1PUB}'" "'${COMMENT}'" "'${MOATS}'"
-' > ${PENDINGDIR}/${MOATS}_replay.sh
-chmod +x ${PENDINGDIR}/${MOATS}_replay.sh
-
-rm -f ${PENDINGDIR}/${MOATS}.result.html
+PENDINGDIR=$HOME/.zen/tmp/${ISSUERPUB}
+mkdir -p $PENDINGDIR
 
 ################################################
 # MAKE PAYMENT
@@ -82,48 +69,26 @@ echo
 echo ${CHK1}
 echo ${CHK2}
 echo
-if [[ ${ISOK} == 0 || $(echo "${CHK2}" | grep 'succès') || $(echo "${CHK1}" | grep 'conforme') ]]; then
+if [[ ${ISOK} == 0 ]]; then
     echo "TRANSACTION SENT"
-    echo "SENT" > ${PENDINGFILE} ## TODO : MONITOR POTENTIAL CHAIN REJECTION (FORK/MERGE WINDOW)
 
-    ## CHANGE COINS CACHE
+    ## ACCESS COINS value CACHE
     COINSFILE="$HOME/.zen/tmp/coucou/${ISSUERPUB}.COINS"
     DESTFILE="$HOME/.zen/tmp/coucou/${G1PUB}.COINS"
 
-    ## DECREASE SOURCE IN "coucou" CACHE
+    ## Apply TX : DECREASE SOURCE IN "coucou" CACHE
+    ## SOURCE WALLET
     echo "$COINS - $AMOUNT" | bc > ${COINSFILE}
 
-    DES=$(cat ${DESTFILE})
+    DES=$(cat ${DESTFILE}) ## DESTINATION WALLET
     [[ ${DES} != "" && ${DES} != "null" ]] \
         && echo "$DES + $AMOUNT" | bc  > ${DESTFILE} \
         || { echo "${AMOUNT}" > ${DESTFILE} && DES=${AMOUNT}; }
 
-    ## INFORM ABOUT PAYMENT
+    ## ZEN CONVERSION
     ZENAMOUNT=$(echo "$AMOUNT * 10" | bc | cut -d '.' -f 1)
     ZENCUR=$(echo "$COINS * 10" | bc | cut -d '.' -f 1)
     ZENDES=$(echo "$DES * 10" | bc | cut -d '.' -f 1)
-
-    ##### ADMIN MONITORING #########
-    echo "<html><head><meta charset='UTF-8'>
-    <title>${COMMENT}</title>
-    <style>
-        body {
-            font-family: 'Courier New', monospace;
-        }
-        pre {
-            white-space: pre-wrap;
-        }
-    </style></head><body>
-    <h1>${ZENAMOUNT} ZEN OPERATION</h1>
-    ${COMMENT}
-    <h3><a title='CESIUM' href='${CESIUMIPFS}/#/app/wot/tx/${ISSUERPUB}/'>${ISSUERPUB}</a>
-    (<a href='$myUPLANET/g1gate/?pubkey=${ISSUERPUB}'>SCAN</a>) : ${ZENCUR} ZEN
-    <br> //--->> <a title='CESIUM' href='${CESIUMIPFS}/#/app/wot/tx/${G1PUB}/'>${G1PUB}</a>
-    (<a href='$myUPLANET/g1gate/?pubkey=${G1PUB}'>SCAN</a>) : ${ZENDES} ZEN
-    </h3>
-    </body></html>" > ${PENDINGDIR}/${MOATS}.result.html
-
-    $MY_PATH/mailjet.sh "support@qo-op.com" ${PENDINGDIR}/${MOATS}.result.html "${ZENAMOUNT} ZEN : ${COMMENT}"
 
 else
 
@@ -144,34 +109,35 @@ else
     ISOK=$?
     CHK1=$(cat ${PENDINGDIR}/${MOATS}.result.html | head -n 1 )
     CHK2=$(cat ${PENDINGDIR}/${MOATS}.result.html | head -n 2 )
-    if [[ ${ISOK} == 0 || $(echo "${CHK2}" | grep 'succès') || $(echo "${CHK1}" | grep 'conforme') ]]; then
+    if [[ ${ISOK} == 0 ]]; then
         echo "TRANSACTION SENT"
-        echo "SENT" > ${PENDINGFILE} ## TODO : MONITOR POTENTIAL CHAIN REJECTION (FORK/MERGE WINDOW)
+        echo "SENT" > ${PENDINGDIR}/${MOATS}.result.html
     fi
-
-    #~ rm ${PENDINGFILE}
-    #~ echo "<html><h2>BLOCKCHAIN CONNEXION ERROR</h2>
-    #~ <h1>-  MUST RETRY -</h1>
-    #~ LAUNCHING SUB SHELL</html>" >> ${PENDINGDIR}/${MOATS}.result.html
-
-    #~ ## COUNT NUMBER OF TRY
-    #~ try=$(cat ${PENDINGDIR}/${MOATS}.try 2>/dev/null) || try=0
-
-    #~ [ $try -gt 2 ] \
-    #~ && echo "${MOATS} TOO MANY TRY ( $try )" >> ${PENDINGDIR}/${MOATS}.result.html \
-    #~ && $MY_PATH/mailjet.sh "support@qo-op.com" ${PENDINGDIR}/${MOATS}.result.html "PAYMENT CANCELED" \
-    #~ && exit 1 \
-    #~ || $MY_PATH/mailjet.sh "support@qo-op.com" ${PENDINGDIR}/${MOATS}.result.html "PAYMENT REPLAY"
-
-   #~ (
-    #~ ((try++)) && echo $try > ${PENDINGDIR}/${MOATS}.try
-    #~ chmod +x  ${PENDINGDIR}/${MOATS}_replay.sh
-    #~ sleep 3600
-    #~ ${PENDINGDIR}/${MOATS}_replay.sh
-    #~ exit 0
-   #~ ) &
-
 fi
+
+
+##### ADMIN MONITORING #########
+echo "<html><head><meta charset='UTF-8'>
+<title>${COMMENT}</title>
+<style>
+    body {
+        font-family: 'Courier New', monospace;
+    }
+    pre {
+        white-space: pre-wrap;
+    }
+</style></head><body>
+<h1>${ZENAMOUNT} ZEN OPERATION</h1>
+${COMMENT}
+<h3><a title='CESIUM' href='${CESIUMIPFS}/#/app/wot/tx/${ISSUERPUB}/'>${ISSUERPUB}</a>
+(<a href='$myUPLANET/g1gate/?pubkey=${ISSUERPUB}'>SCAN</a>) : ${ZENCUR} ZEN
+<br> //--->> <a title='CESIUM' href='${CESIUMIPFS}/#/app/wot/tx/${G1PUB}/'>${G1PUB}</a>
+(<a href='$myUPLANET/g1gate/?pubkey=${G1PUB}'>SCAN</a>) : ${ZENDES} ZEN
+</h3>
+</body></html>" >> ${PENDINGDIR}/${MOATS}.result.html ## SEND FULL LOG simply é>é to stop
+
+$MY_PATH/mailjet.sh "support@qo-op.com" ${PENDINGDIR}/${MOATS}.result.html "${ZENAMOUNT} ZEN : ${COMMENT}"
+
 
 ## REMOVE IF YOU WANT TO MONITOR "SENT" WINDOW INCERTITUDE
 rm -Rf ${PENDINGDIR}
