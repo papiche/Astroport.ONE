69dfde3f27010bb408563736f1702c0b3a41b819
fred <support@qo-op.com>
2025-08-08 04:44:44 +0200

    pfffssss

--- tools/make_NOSTRCARD.sh
+++ tools/make_NOSTRCARD.sh
@@ -189,7 +189,7 @@ if [[ $EMAIL =~ ^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$ ]]; then
 
     ## Make PLAYER "SSSS.head:NOSTRNS" QR CODE (Terminal Compatible) - "M-$SSSS_HEAD_B58"
     SSSS_HEAD=$(cat ~/.zen/tmp/${MOATS}/${EMAIL}.ssss.head)
-    SSSS_HEAD_B58=$(${MY_PATH}/base58.py encode "${SSSS_HEAD}:$NOSTRNS")
+    SSSS_HEAD_B58=$(${MY_PATH}/Mbase58.py encode "${SSSS_HEAD}:$NOSTRNS")
     amzqr "M-$SSSS_HEAD_B58" -l H -p ${MY_PATH}/../templates/img/key.png \
         -c -n ._SSSSQR.png -d ~/.zen/game/nostr/${EMAIL}/ &>/dev/null
 
