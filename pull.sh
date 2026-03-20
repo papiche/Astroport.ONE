0425c97be9f3d51f50ba7929306736655d09a769
FRD <fred@linkeo.com>
2026-03-20 20:40:58 +0100

    pull all code

--- pull.sh
+++ pull.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+# Sauvegarde du répertoire courant au début du script
+ORIGINAL_DIR="$PWD"
+
+# Astroport.ONE - task & process scheduler
 cd ~/.zen/Astroport.ONE
 git pull
-cd -
-cd ~/.zen/workspace/NIP-101
-git pull
-cd -
+
+# 54321 API
 cd ~/.zen/UPassport
 git pull
-cd -
+
+# UPlanet - /ipns/copylaradio.com
+cd ~/.zen/workspace/UPlanet
+git pull
+
+# NIP101 implementation // strfry plugin rules
+cd ~/.zen/workspace/NIP-101
+git pull
+
+# OC2UPlanet €/Ẑ interface
 cd ~/.zen/workspace/OC2UPlanet
 git pull
-cd -
 
+# Retour au répertoire d'origine
+cd "$ORIGINAL_DIR"
